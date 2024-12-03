ae9bc22b2cae06e862b7a19aa2ade596fe5da25c
Hugo <54435874+Cactooz@users.noreply.github.com>
2024-12-03 22:50:06 +0100

    Add Day2 Part2

--- day2/day2.py
+++ day2/day2.py
@@ -1,14 +1,25 @@
 file = open("./day2", "r")
 
 safeCount = 0
+safeCountDampened = 0
+
+def safe(numbers):
+    diffs = [numbers[i + 1] - numbers[i] for i in range(len(numbers) - 1)]
+    return (all(1 <= abs(diff) <= 3 for diff in diffs) and
+            (all(diff >= 0 for diff in diffs) or all(diff <= 0 for diff in diffs)))
 
 for line in file:
     numbers = list(map(int, line.strip().split()))
-    diffs = [numbers[i + 1] - numbers[i] for i in range(len(numbers) - 1)]
 
-    if (all(1 <= abs(diff) <= 3 for diff in diffs) and
-            (all(diff >= 0 for diff in diffs) or all(diff <= 0 for diff in diffs))):
+    if safe(numbers):
         safeCount += 1
         continue
 
+    for index in range(len(numbers)):
+        dampenedNumbers = numbers[:index] + numbers[index + 1:]
+        if safe(dampenedNumbers):
+            safeCountDampened += 1
+            break
+
 print(safeCount)
+print(safeCount + safeCountDampened)
